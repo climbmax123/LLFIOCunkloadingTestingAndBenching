048556b2925679f9d8656a75c1d8e7c1a3dc3ba5
Lukas Karafiat <minimalarchitect.github@gmail.com>
2024-02-20 19:15:31 +0100

    initial commit
    added dependencies on:
    - ned14/llfio
    - ned14/outcome
    - ned14/quickcpplib
    - nlohmann/json

---
+++ linux-install-dep.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+git submodule update --recursive --remote
+
+cd lib/json || exit 1
+mkdir -p build
+cd build || exit 1
+cmake ..
+make -j16
+sudo make install
+cd ..
+cd ../..
+
+
+cd lib/quickcpplib || exit 1
+mkdir build
+cd build || exit 1
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . -- _hl -j 16
+make -j16
+sudo make install
+cd ..
+cd ../..
+
+
+cd lib/outcome || exit 1
+mkdir build
+cd build || exit 1
+cmake ..
+make -j16
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . -- _hl -j 16
+cd ..
+cd ../..
+
+cd lib/llfio || exit 1
+mkdir build
+cd build || exit 1
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . -- _dl -j 16
+make -j16 -DLLFIO_HEADERS_ONLY=0 -DLLFIO_DYN_LINK
+sudo make install
+cd ..
+cd ../..
